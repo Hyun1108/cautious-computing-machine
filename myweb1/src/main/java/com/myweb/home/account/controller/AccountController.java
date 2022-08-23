27c47da9bd2613d1e002aac5bb0363bc7d3832e4
안병현 <abhyuni@gmail.com>
2022-08-23 13:43:00 +0900

    회원가입 기능 구현중

--- myweb1/src/main/java/com/myweb/home/account/controller/AccountController.java
+++ myweb1/src/main/java/com/myweb/home/account/controller/AccountController.java
@@ -1,5 +1,23 @@
 package com.myweb.home.account.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.myweb.home.account.model.AccountDTO;
+
+@Controller
+@RequestMapping(value="/account")
 public class AccountController {
 
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		return "account/join";
+	}
+	
+	@RequestMapping(value="join", method=RequestMethod.GET)
+	public String join(AccountDTO accountDto) {
+		return "redirect:/";
+	}
+	
 }
